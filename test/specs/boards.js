1379fa7d11ea8c76952deb2730d338ef586a2446
Wendreo <wendreosantos@softbox.com.br>
2019-02-07 18:41:23 -0200

    teste final do dia

--- test/specs/boards.js
+++ test/specs/boards.js
@@ -9,26 +9,27 @@ describe('Boards Trello', function () {
         loginPage.logar("qachallengesoftbox@email.com", "S0ftbox123");
         expect(basePage.getNomeUsuario()).to.equal('QA_CHALLENGE_SOFTBOX (qa_challenge_softbox)');
         boardListPage.open();
-        boardListPage.criarQuadro("Gerenciamento de projetos 2019");
-        expect(boardPage.getNomeQuadro()).to.equal("Gerenciamento de projetos 2019");
+        boardListPage.criarQuadro("Projeto automacao");
+        expect(boardPage.getNomeQuadro()).to.equal("Projeto automacao");
 
         
     });
 
-             
+            
     
-    it("Removendo o board criado acima", function() {
+    it("Removendo o board criado acima", function() {   
+     
+         browser.click ('.board-menu-navigation-item-link.js-open-more');
+         browser.click ('.js-close-board');
+         browser.click ('.js-confirm.full.negate');
+         browser.click ('.quiet.js-delete');
+         browser.click ('.js-confirm.full.negate');
+         //browser.click ('classic > div.pop-over.is-shown > div > div:nth-child(2) > div > div > div > input');
+         browser.click ('.header-btn-text');
+        
 
-        browser.click ('[title="Gerenciamento de projetos 2019"]');
-        browser.click ('.mod-show-menu');
-
-        Thread.sleep(8000);
         /**
-         * Criar o script que faz a remoção do board criado acima, Gerencimaneto de projetos 2019
-         * 1. Acessar página de lista de boards
-         * 2. Clicar no quadro Gerenciamento de projetos 2019
-         * 3. No menu do lado direito, clicar em mais, e no botão fechar quadro
-         * 4. Confirma fechar o quadro no modal que se abre
+         
          * 5. Confirma mensagem de sucesso que o quadro está fechado
          * 6. Acessar página da lista de quadros novamente e verificar se de fato o quadro foi removido
          */
